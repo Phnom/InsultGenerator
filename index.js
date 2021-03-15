ac66577b6670eab1eabb6fdb5c96558aeeb024c9
Phnome <soold88@hotmail.se>
2021-03-15 12:00:14 +0100

    searchFeatures

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express")
+require("dotenv").config()
+
+const app = express()
+const PORT = process.env.Port || 3000
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+  console.log(req.query)
+  const insultList = require("./src/insults.json")
+  random = Math.floor(Math.random() * insultList.insults.length)
+  res.render("index", {
+    insult: insultList.insults[random],
+  })
+})
+
+app.get("/search", (req, res) => {
+  let insultList = require("./src/insults.json").insults
+  if (
+    req.query.text.length > 0 &&
+    insultList.filter((insults) => insults.text.includes(req.query.text))
+      .length > 0
+  ) {
+    insultList = insultList.filter((insults) =>
+      insults.text.includes(req.query.text)
+    )
+  }
+  if (
+    req.query.origin.length > 0 &&
+    insultList.filter((insults) => insults.origin.includes(req.query.origin))
+      .length > 0
+  ) {
+    console.log(
+      insultList.filter((insults) => insults.origin.includes(req.query.origin))
+    )
+    insultList = insultList.filter((insults) =>
+      insults.origin.includes(req.query.origin)
+    )
+  }
+  if (req.query.severity.length > 0) {
+    insultList = insultList.filter(
+      (insults) => insults.severity == req.query.severity
+    )
+  }
+  res.render("index", {
+    insult: insultList[0],
+  })
+})
+
+app.listen(PORT, function () {
+  console.log(`Server started on port ${PORT}`)
+})
